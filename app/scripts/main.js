936d1d1c001f02fbc1bf39a16c9c65ad2f7fed54
Thanh Khoa Nguyen <thanhkhoa.nguyen@ogilvy.com>
2018-10-16 15:00:08 +0700

    adding detail page

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,6 +47,24 @@ $(document).ready(function() {
         showPopUp();
       }
     });
+  });
+
+  $('#fbShare').on('click', function(e) {
+    e.preventDefault();
+    FB.ui({
+      method: 'share_open_graph',
+      action_type: 'og.likes',
+      action_properties: JSON.stringify({
+        object: {
+          image:'https://ct2018.asiadigitalhub.com/sites/default/files/101556094912508251539674674_5331.jpg',
+          url: 'https://ct2018.asiadigitalhub.com',
+          title: fd.name,
+          description: fd.quote
+        }
+      })
+    }, function(response) {
+      console.log(repsonse)
+    })
   })
 
   $('#reset').on('click', function(e) {
