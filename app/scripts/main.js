6c06d3c29ad7e0d725e0aa3ff67500ede24310b5
Thanh Khoa Nguyen <thanhkhoa.nguyen@ogilvy.com>
2018-10-16 08:06:36 +0700

    update petal

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,10 @@
 $(document).ready(function() {
+  // parallax
+  var scene = document.getElementById('petal')
+  var parallaxInstance = new Parallax(scene);
+
+
+
   var base64;
 
   $('#btn-start').on('click', function(e) {
@@ -53,9 +59,9 @@ $(document).ready(function() {
     if (response.status === 'connected') {
       getFbUserData(accessToken);
     } else if (response.status === 'not_authorized') {
-      console.log("Err");
+      console.log('Err');
     } else {
-      console.log("Err");
+      console.log('Err');
     }
   }
 
@@ -85,44 +91,44 @@ $(document).ready(function() {
 
   function postImageToFacebook(token, filename, mimeType, imageData, message) {
     var fd = new FormData();
-    fd.append("access_token", token);
-    fd.append("source", imageData);
-    fd.append("no_story", true);
+    fd.append('access_token', token);
+    fd.append('source', imageData);
+    fd.append('no_story', true);
 
     // Upload image to facebook without story(post to feed)
     $.ajax({
-      url: "https://graph.facebook.com/me/photos?access_token=" + token,
-      type: "POST",
+      url: 'https://graph.facebook.com/me/photos?access_token=' + token,
+      type: 'POST',
       data: fd,
       processData: false,
       contentType: false,
       cache: false,
       success: function (data) {
-        console.log("success: ", data);
+        console.log('success: ', data);
         // Get image source url
         FB.api(
-          "/" + data.id + "?fields=images",
+          '/' + data.id + '?fields=images',
           function (response) {
             if (response && !response.error) {
               //console.log(response.images[0].source);
 
               // Create facebook post using image
               FB.api(
-                "/me/feed",
-                "POST", {
-                  "message": "",
-                  "picture": response.images[0].source,
-                  "link": window.location.href,
-                  "name": 'Look at the cute panda!',
-                  "description": message,
-                  "privacy": {
+                '/me/feed',
+                'POST', {
+                  'message': '',
+                  'picture': response.images[0].source,
+                  'link': window.location.href,
+                  'name': 'Look at the cute panda!',
+                  'description': message,
+                  'privacy': {
                     value: 'SELF'
                   }
                 },
                 function (response) {
                   if (response && !response.error) {
                     /* handle the result */
-                    console.log("Posted story to facebook");
+                    console.log('Posted story to facebook');
                   }
                 }
               );
@@ -131,7 +137,7 @@ $(document).ready(function() {
         );
       },
       error: function (shr, status, data) {
-        console.log("error " + data + " Status " + shr.status);
+        console.log('error ' + data + ' Status ' + shr.status);
       },
       complete: function (data) {
         //console.log('Post to facebook Complete');
@@ -172,13 +178,13 @@ $(document).ready(function() {
     var placeholder = new Image(500, 600);
     placeholder.src = '../images/placeholder.jpg';
     var ava = new Image(165, 165);    
-    ava.crossOrigin="anonymous";
+    ava.crossOrigin='anonymous';
     var resultImg = document.getElementById('result-image')
 
     placeholder.onload = function() {
       ctx.drawImage(placeholder, 0, 0)
-      ctx.font = "30px 'Cormorant Garamond'";
-      ctx.textAlign = "center";
+      ctx.font = '30px \'Cormorant Garamond\'';
+      ctx.textAlign = 'center';
       ctx.fillText(fullName.toUpperCase(), canvas.width/2, 90);
 
       ava.src = response.picture.data.url;
