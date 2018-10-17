9894df5171fad9461621d26b384a906506d17a29
Thanh Khoa Nguyen <thanhkhoa.nguyen@ogilvy.com>
2018-10-17 22:59:33 +0700

    update new result photo size

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,16 +1,14 @@
 $(document).ready(function() {
   var canvas = document.createElement('canvas')
-  canvas.width = 500
-  canvas.height = 600
   var ctx = canvas.getContext('2d');
-  var ava = new Image(165, 165);    
+  var ava = new Image(165, 165);
   ava.crossOrigin='anonymous';
   var base64;
   var resultImg = document.getElementById('result-image');
   if(resultImg) {
     var initImageSrc = resultImg.src;
   }
-  var placeholder = new Image(500, 600);
+  var placeholder = new Image();
   var teams = [
     'CÁ TÍNH', 'SÀNH ĐIỆU', 'SANG CHẢNH', 'GỢI CẢM'
   ]
@@ -25,9 +23,9 @@ $(document).ready(function() {
     quote: '',
     photo: ''
   };
-  var quotes; 
+  var quotes;
   $.getJSON( 'https://ct2018.asiadigitalhub.com/api/ct/matrix', function( data ) {
-    quotes = data; 
+    quotes = data;
   });
 
 
@@ -178,7 +176,7 @@ $(document).ready(function() {
   function getFbUserData() {
     FB.api('/me', {
         locale: 'en_US',
-        fields: 'id,first_name,last_name,email,link,gender,locale,picture.width(165).height(165)'
+        fields: 'id,first_name,last_name,email,link,gender,locale,picture.width(260).height(260)'
       },
       function (response) {
         setupCanvas(response)
@@ -189,22 +187,41 @@ $(document).ready(function() {
   function setupCanvas(response) {
     var fullName = response.first_name + ' ' + response.last_name;
     placeholder.src = initImageSrc;
+    var teamName = teams[fd.team - 1];
+    var scentName = scents[fd.scent - 1];
 
     placeholder.onload = function() {
+      // ctx.fillText(fullName.toUpperCase(), canvas.width/2, 90);
+
+
+      // generate share fb photo
+      // ctx.font = '500 35px \'CormorantGaramond-Medium\'';
+      // ctx.fillText(fullName.toUpperCase(), 303, 290);
+
+      // ctx.font = '500 21px \'CormorantGaramond-Medium\'';
+      // ctx.textAlign='end';
+      // ctx.fillText(teams[fd.team - 1], canvas.width - 200, canvas.height - 229);
+      // ctx.textAlign='start';
+      // ctx.fillText(scents[fd.scent - 1], canvas.width/2 - 40, canvas.height - 202);
+
+
+      // generate result phoyo
+      canvas.width = 388 ;
+      canvas.height = 388;
       ctx.drawImage(placeholder, 0, 0)
-      ctx.font = '500 25px \'CormorantGaramond-Medium\'';
-      ctx.textAlign = 'center';
-      ctx.fillText(fullName.toUpperCase(), canvas.width/2, 90);
-      
-      ctx.font = '500 21px \'CormorantGaramond-Medium\'';
-      ctx.textAlign='end'; 
-      ctx.fillText(teams[fd.team - 1], canvas.width - 200, canvas.height - 229);
-      ctx.textAlign='start'; 
-      ctx.fillText(scents[fd.scent - 1], canvas.width/2 - 40, canvas.height - 202);
+      ctx.font = '500 22px \'CormorantGaramond-Medium\'';
+      ctx.textAlign = 'start';
+      ctx.fillText(fullName.toUpperCase(), 80, 50);
+
+      ctx.textAlign = 'end';
+      ctx.font = '500 13px \'CormorantGaramond-Medium\'';
+      ctx.fillText(teamName, canvas.width/2 + 37, 267);
+      ctx.textAlign = 'start';
+      ctx.fillText(scentName, canvas.width/2 - 31, 285);
 
       ava.src = response.picture.data.url;
       ava.onload = function() {
-        ctx.drawImage(ava, 0, 0, 165, 165, 50, 125, 165, 165);
+        ctx.drawImage(ava, 0, 0, 260, 260, 45, 65, 120, 120);
         base64 = canvas.toDataURL();
         resultImg.src = base64;
         fd.photo = base64;
@@ -233,5 +250,5 @@ $(document).ready(function() {
     });
   }
 
-  
+
 })
